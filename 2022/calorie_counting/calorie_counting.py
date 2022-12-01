a68ffef131fdf4423f2f64263d6ef1e9e5687559
ailac22 <imorenoval@protonmail.com>
2022-12-01 08:38:27 +0100

    Day 1 done!

--- 2022/calorie_counting/calorie_counting.py
+++ 2022/calorie_counting/calorie_counting.py
@@ -4,10 +4,13 @@ def parse_input(f):
     lines = f.read().split("\n")
     return [int(x) if x.isdigit() else 0 for x in lines ]
 
-def find_elf(input_list):
+def calories_list(input_list):
     L = [list(g) for _, g in itertools.groupby(input_list, key=lambda x: x == 0)]
-    return max([sum(x) for x in L if x])
+    res = [sum(x) for x in L if x]
+    res.sort(reverse=True)
+    return res
 
 with open("input.txt","r") as f:
     L = parse_input(f) 
-    print(find_elf(L))
+    print(f"Top: {calories_list(L)[0]}")
+    print(f"Top 3: {sum(calories_list(L)[0:3])}")
