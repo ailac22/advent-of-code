28338b863cd3dec1bdf6d751f6b875c0ad8f9f5c
ailac22 <imorenoval@protonmail.com>
2022-12-02 07:32:27 +0100

    That went worse that I expected, but we got it

--- 2022/rock_paper_scisors/rock_paper_scissors.py
+++ 2022/rock_paper_scisors/rock_paper_scissors.py
@@ -1,28 +1,64 @@
 import itertools
 
-# Rock: A X
-# Paper: B Y
-# Scissors: C Z
 
-rpsValue = {'X':1, 'Y':2, 'Z': 3}
-wdlValue = [6,3,0]
-rpsWC = {'A': 'Z', 'B': 'X', 'C': 'Y', 'X':'C', 'Y': 'A', 'Z': 'B'}
+rpsValue = {'A':1, 'B':2, 'C': 3}
+rpsWC = {'A': 'C', 'B': 'A', 'C':'B' }
+rpsLC = {val: key for (key, val) in rpsWC.items()}
 
-def play_round(game):
-    if len(game) < 2: return 0;
-    roundOutcome = rpsValue[game[1]] 
+def roundOutcome(game):
+
+    wdlValue = [6,3,0]
     if rpsWC[game[0]] == game[1]:
-        roundOutcome = roundOutcome + wdlValue[2]
+        return wdlValue[2]
     elif rpsWC[game[1]] == game[0]:
-        roundOutcome = roundOutcome + wdlValue[0]
+        return  wdlValue[0]
     else:
-        roundOutcome = roundOutcome + wdlValue[1]
-    return roundOutcome 
+        return wdlValue[1]
 
+def play_round_strategy_1(game):
+    equivalences = {'X': 'A', 'Y': 'B', 'Z': 'C'}
+    # Rock: A X
+    # Paper: B Y
+    # Scissors: C Z
+    #rpsWC = {'A': 'Z', 'B': 'X', 'C': 'Y', 'X':'C', 'Y': 'A', 'Z': 'B'}
+    if len(game) < 2: return 0;
+    transformed_game = game 
+    transformed_game[1] = equivalences[game[1]]
+    return roundOutcome(game) + rpsValue[game[1]] 
+
+
+def play_round_strategy_2(game):
+    
+    # Rock: A 
+    # Paper: B 
+    # Scissors: C 
+    # rpsNeed2Do = {'X': 2, 'Y':1, 'Z': 0}
+
+    
+    if len(game) < 2: return 0;
+    selected = '?'
+    print("game")
+    print(game)
+    if game[1] == 'X':
+        selected = rpsWC[game[0]]
+    elif game[1] == 'Z':
+        selected = rpsLC[game[0]]
+    else:
+        selected = game[0]
+
+    print("selected")
+    print(selected)
+    return rpsValue[selected] + roundOutcome([game[0],selected])
+
+    # roundOutcome = rpsValue[game[1]] 
+    # if rpsWC[game[1]] == 'X':
+    #     roundOutcome += wdlValue[2]
 
 with open("input.txt") as f:
     L = [x.split(" ") for x in f.read().split("\n")]
 
     print(L)
-    res = sum(map(play_round,L))
-    print(res)
+    # res1 = sum(map( play_round_strategy_1,L))
+    # print(f"first strategy: {res1}")
+    res2 = sum(map(play_round_strategy_2,L))
+    print(f"second strategy: {res2}")
