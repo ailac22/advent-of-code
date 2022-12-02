ae7fcb3357cfc9cc8d16c04739378c5640e5bb49
ailac22 <imorenoval@protonmail.com>
2022-12-02 07:43:36 +0100

    Some cleaning, and day 2 is Done

--- 2022/rock_paper_scisors/rock_paper_scissors.py
+++ 2022/rock_paper_scisors/rock_paper_scissors.py
@@ -1,12 +1,10 @@
 import itertools
 
-
 rpsValue = {'A':1, 'B':2, 'C': 3}
 rpsWC = {'A': 'C', 'B': 'A', 'C':'B' }
 rpsLC = {val: key for (key, val) in rpsWC.items()}
 
 def roundOutcome(game):
-
     wdlValue = [6,3,0]
     if rpsWC[game[0]] == game[1]:
         return wdlValue[2]
@@ -17,28 +15,11 @@ def roundOutcome(game):
 
 def play_round_strategy_1(game):
     equivalences = {'X': 'A', 'Y': 'B', 'Z': 'C'}
-    # Rock: A X
-    # Paper: B Y
-    # Scissors: C Z
-    #rpsWC = {'A': 'Z', 'B': 'X', 'C': 'Y', 'X':'C', 'Y': 'A', 'Z': 'B'}
-    if len(game) < 2: return 0;
-    transformed_game = game 
-    transformed_game[1] = equivalences[game[1]]
-    return roundOutcome(game) + rpsValue[game[1]] 
+    t_game = [game[0],equivalences[game[1]]]
+    return roundOutcome(t_game) + rpsValue[t_game[1]] 
 
 
 def play_round_strategy_2(game):
-    
-    # Rock: A 
-    # Paper: B 
-    # Scissors: C 
-    # rpsNeed2Do = {'X': 2, 'Y':1, 'Z': 0}
-
-    
-    if len(game) < 2: return 0;
-    selected = '?'
-    print("game")
-    print(game)
     if game[1] == 'X':
         selected = rpsWC[game[0]]
     elif game[1] == 'Z':
@@ -46,19 +27,11 @@ def play_round_strategy_2(game):
     else:
         selected = game[0]
 
-    print("selected")
-    print(selected)
     return rpsValue[selected] + roundOutcome([game[0],selected])
 
-    # roundOutcome = rpsValue[game[1]] 
-    # if rpsWC[game[1]] == 'X':
-    #     roundOutcome += wdlValue[2]
-
 with open("input.txt") as f:
-    L = [x.split(" ") for x in f.read().split("\n")]
-
-    print(L)
-    # res1 = sum(map( play_round_strategy_1,L))
-    # print(f"first strategy: {res1}")
+    L = [x.split(" ") for x in f.read().split("\n")[:-1]]
+    res1 = sum(map( play_round_strategy_1,L))
+    print(f"first strategy: {res1}")
     res2 = sum(map(play_round_strategy_2,L))
     print(f"second strategy: {res2}")
