a341e2442748d3b1abe4edddfed00d38810cfb9d
ailac22 <imorenoval@protonmail.com>
2022-12-10 01:33:56 +0100

    Second part went actually bad 😂. Didn't do the correct move strategy and had to go to work. Next in 4h.

--- 2022/rope_bridge/rope_bridge.py
+++ 2022/rope_bridge/rope_bridge.py
@@ -1,13 +1,3 @@
-
-
-H = (0,0)
-H_prev = (0,0)
-T = (0,0)
-
-rope = [(0,0)]*9
-print(f"rope:{rope}")
-T_positions = set() 
-
 def vector_sum(a,b):
     import operator
     return tuple(map(operator.add, a, b))
@@ -16,24 +6,26 @@ def vector_sub(a,b):
     import operator
     return tuple(map(operator.sub, a, b))
 
-def tightRope(a,b):
+def tight_knot(a,b):
     distance = vector_sub(a,b)
     return distance[0] > 1 or distance[1] > 1 or distance[0] < -1 or distance[1] < -1 
 
-def moveH(mymov):
-    global H
-    global H_prev
-    global T
+def displacement_dir(a,b):
+    dist = vector_sub(a,b)
+    return (max(-1, min(1, dist[0])),max(-1, min(1, dist[1])))
 
+def move_rope(mymov, rope, t_positions):
     for _ in range(mymov[2]):
-        H_prev = H
-        H = vector_sum(H,mymov[:2])
-        if tightRope(H,T):
-            T = H_prev
-            T_positions.add(T)
+        rope[0] = vector_sum(rope[0],mymov[:2])
+        for i in range(1,len(rope)):
+            if tight_knot(rope[i],rope[i-1]):
+                rope[i] = vector_sum(rope[i],displacement_dir(rope[i-1],rope[i]))
+        t_positions.add(rope[-1])
         
+    
 def transfMov(origMov):
     times = origMov[1]
+
     match origMov[0]:
         case 'R':
             return (1,0,times)
@@ -44,12 +36,20 @@ def transfMov(origMov):
         case 'D':
             return (0,-1,times)
 
-with open('input.txt','r') as f:
-    L = [ (x.split()[0], int(x.split()[1])) for x in f.read().strip().split('\n')]
-    
+def getTailPositions(L,rope,t_positions):
     for move in L:
         transMov = transfMov(move)
-        moveH(transMov)
+        move_rope(transMov,rope,t_positions)
+    return t_positions
 
-    print(len(T_positions))
+with open('input.txt','r') as f:
+    L = [ (x.split()[0], int(x.split()[1])) for x in f.read().strip().split('\n')]
 
+    rope1 = [(int(0),int(0))]*2
+    rope2 = [(int(0),int(0))]*10
+
+    t_positions_1 = set() 
+    t_positions_2 = set() 
+
+    print(f"First solution: {len(getTailPositions(L,rope1,t_positions_1))}")
+    print(f"Second solution: {len(getTailPositions(L,rope2,t_positions_2))}")
